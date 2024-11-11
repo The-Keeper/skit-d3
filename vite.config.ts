e4bd059b8a51cada2c8326dbeaf8be3cf8ac4a43
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-11-12 04:50:15 +0500

    use vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import { defineConfig } from 'vite';
 import unocss from 'unocss/vite';
 
 export default defineConfig({
-	plugins: [unocss(), sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	plugins: [unocss(), sveltekit()]
 });
